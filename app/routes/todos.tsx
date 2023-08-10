cfec439965db738b46372d2c5f314a2b15b54f82
Kirill Chernyshev <kchernyshev@outlook.com>
2023-08-10 20:45:22 +0400

    Clean up

--- app/routes/todos.tsx
+++ app/routes/todos.tsx
@@ -23,7 +23,6 @@ export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData();
 
   const title = formData.get("title") as string;
-  console.log("title", title);
   return await createTodo({ title });
 };
 
