865fa001122e1fb4f8e51b26b6bba8e6baff4912
Kirill Chernyshev <kchernyshev@outlook.com>
2023-08-10 13:14:56 +0400

    Migrate DB

---
+++ prisma/migrations/20230809201940_init2/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Note` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "Note";
+PRAGMA foreign_keys=on;
+
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Todo_title_key" ON "Todo"("title");
